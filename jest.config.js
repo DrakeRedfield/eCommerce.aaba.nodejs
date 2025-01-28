6a94a3ec26552df196b10f6bdd8766bbea84b828
Drake Redfield <drake.redfield@hotmail.com>
2025-01-28 16:48:06 -0600

    feat: add auth unit testing

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
+  "testMatch": [
+    "<rootDir>/src/test/**/*.test.ts",
+  ],
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
